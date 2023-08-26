4abc887448f73cdd4aba5bd9ec55ff4690ba011b
andremelinski <andremelinski29@gmail.com>
2023-08-26 09:54:00 -0300

    finishing tcp server examples

---
+++ web-dev-todd/servers/08-building-tcp-http/hands-on/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer listener.Close()
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Fatalln(err.Error())
+			continue
+		}
+
+		go handle(connection)
+	}
+
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	request(conn)
+
+	respond(conn)
+}
+
+func request(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	i := 0
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if ln == "" {
+			// headers are done
+			fmt.Println("done")
+			break
+		}
+
+		if i == 0 {
+
+			// request line
+			fields := strings.Fields(ln)
+			fmt.Println("***METHOD", fields[0])
+			fmt.Println("***URl", fields[1])
+		}
+		i++
+
+	}
+}
+
+func respond(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>Hello World</strong></body></html>`
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
