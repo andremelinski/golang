fb11a16f31959b285066fa2d76fb7e5d5d45cedc
andremelinski <andremelinski29@gmail.com>
2023-10-27 13:25:20 -0300

    update album route

--- web-dev-todd/servers/15-db/main.go
+++ web-dev-todd/servers/15-db/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"reflect"
+	"strings"
 	"time"
 
 	"github.com/asaskevich/govalidator"
@@ -33,6 +36,8 @@ type IAlbumRepository interface{
 
 var db *sql.DB
 var err error
+
+var albumDB IAlbumDB
 
 func init(){
 	// https://hackwild.com/article/go-input-validation-and-testing/
@@ -61,6 +66,8 @@ func main() {
         log.Fatal(pingErr)
     }
     fmt.Println("Connected!")
+    defer db.Close()
+    
     HandleRequests()    
 }
 
@@ -68,8 +75,8 @@ func HandleRequests(){
     mux := httprouter.New()
 	mux.POST("/", CreateAlbum)
 	mux.GET("/", GetAllAlbums)
-	// mux.GET("/album/:id", GetAlbumById)
-    // mux.PUT("/album/:id", UpdateAlbum)
+	mux.GET("/album/:id", GetAlbumById)
+    mux.PUT("/album/:id", UpdateAlbum)
 	// mux.DELETE("/album/:id", DeleteAlbum)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
@@ -88,7 +95,7 @@ func CreateAlbum(res http.ResponseWriter, req *http.Request, _ httprouter.Params
 		return 
 	}
 
-   newAlbumId, err := albumData.DbCreateAlbm()
+   newAlbumId, err := albumDB.DbCreateAlbm(albumData)
    if(err != nil) {
         http.Error(res, err.Error(), 500)
         return 
@@ -100,8 +107,7 @@ func CreateAlbum(res http.ResponseWriter, req *http.Request, _ httprouter.Params
 }
 
 func GetAllAlbums(res http.ResponseWriter, req *http.Request, _ httprouter.Params){
-
-    albums, err := DbLoadAllAlbums()
+    albums, err := albumDB.DbLoadAllAlbums()
     if(err != nil) {
         http.Error(res, err.Error(), 500)
         return 
@@ -111,7 +117,39 @@ func GetAllAlbums(res http.ResponseWriter, req *http.Request, _ httprouter.Param
 	json.NewEncoder(res).Encode(albums)
 }
 
-func (albumData IAlbumProps)DbCreateAlbm()(int64, error){
+func GetAlbumById(res http.ResponseWriter, _ *http.Request, param httprouter.Params){
+    album, err := albumDB.DbLoadAlbumById(param.ByName("id"))
+    if(err != nil) {
+        http.Error(res, err.Error(), 500)
+        return 
+    }
+    res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	json.NewEncoder(res).Encode(album)
+}
+
+func UpdateAlbum(res http.ResponseWriter, req *http.Request, param httprouter.Params) {
+    albumId := param.ByName("id")
+    reqBody, _ := io.ReadAll(req.Body)
+
+	albumData := IAlbumProps{}
+	if err := json.Unmarshal(reqBody, &albumData); err != nil {
+        http.Error(res, err.Error(), 500)
+		return 
+    }
+
+   albumUpdated, err := albumDB.DbUpdateAlbum(albumData, albumId)
+   if(err != nil) {
+        http.Error(res, err.Error(), 500)
+        return 
+    }
+
+    res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	json.NewEncoder(res).Encode(albumUpdated)
+}
+
+func (albumDB IAlbumDB)DbCreateAlbm(albumData IAlbumProps)(int64, error){
      newAlbum, err := db.Exec("INSERT INTO album(title, artist, price) VALUES (?,?,?)", 
     albumData.Title, albumData.Artist, albumData.Price)
 
@@ -121,13 +159,13 @@ func (albumData IAlbumProps)DbCreateAlbm()(int64, error){
 
     id, err := newAlbum.LastInsertId()
     if err != nil {
-        return 0, fmt.Errorf("addAlbum: %v", err)
+        return 0, fmt.Errorf("LastInsertId in addAlbum: %v", err)
     }
     return id, nil
 }
 
-func DbLoadAllAlbums()([]IAlbumDB, error){
-    albums := []IAlbumDB{}
+func(albumDb IAlbumDB) DbLoadAllAlbums()(*[]IAlbumDB, error){
+    albums := &[]IAlbumDB{}
 
     rows, err := db.Query("SELECT * FROM album")
     if err != nil {
@@ -140,10 +178,62 @@ func DbLoadAllAlbums()([]IAlbumDB, error){
         if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
             return nil, err
         }
-        albums = append(albums, alb)
+        *albums = append(*albums, alb)
     }
 
     return albums, nil
+}
+
+func(albumDb IAlbumDB) DbLoadAlbumById(id string)(*IAlbumDB, error){
+    row := db.QueryRowContext(context.TODO(), "SELECT * FROM album WHERE id=?", id)
+    err := row.Scan(&albumDb.ID, &albumDb.IAlbumProps.Title, &albumDb.IAlbumProps.Artist, &albumDb.IAlbumProps.Price)
+    if err != nil {
+        return nil, err
+    }
+    return &albumDb, nil
+}
+
+func(albumDb IAlbumDB) DbUpdateAlbum(albumData IAlbumProps, id string)(int64, error){
+    fieldsArr := reflect.TypeOf(albumData)
+    valuesArr := reflect.ValueOf(albumData)
+    fieldsNum := fieldsArr.NumField()
+    updateConditition:= "UPDATE album SET "
+
+    for i := 0; i < fieldsNum; i++ {
+        field := fieldsArr.Field(i)
+        fieldName := strings.ToLower(field.Name)
+        value := valuesArr.Field(i)
+        switch field.Type.Kind(){
+        case reflect.String:
+            if value.String() != "" {
+                stringCondition := fieldName+"="+"'"+value.String()+"', "
+                updateConditition += stringCondition
+            }
+    default:
+        floatValue := value.Float()
+        if floatValue > 0 {
+                stringCondition := fieldName+"="+ fmt.Sprintf("%.2f", floatValue)+", "
+                // stringCondition := fieldName+"="+ strconv.FormatFloat(floatValue, 'f', -1, 64)
+                updateConditition += stringCondition
+            }
+        }
+    }
+    updateConditition = updateConditition[:len(updateConditition)-2] + " WHERE id="+id+";"
+    // updateConditition = strings.Replace(updateConditition, ", WHERE", " WHERE", -1)
+
+    albumUpdated, err := db.Exec(updateConditition)
+
+        if err != nil {
+        return 0, fmt.Errorf("addAlbum: %v", err)
+    }
+
+	row, err := albumUpdated.RowsAffected()
+
+    if err != nil {
+            return 0, fmt.Errorf("RowsAffected in updateAlbum: %v", err)
+        }
+    return row, nil
+    
 }
 
 func validator[T any](dataObj T)error{
